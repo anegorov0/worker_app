be136411a173036e0969df81a5e7ce60e4b642b3
anegorov0 <signyts@gmail.com>
2025-07-14 12:49:10 +0300

    add base CRUD

--- app/Http/Controllers/WorkerController.php
+++ app/Http/Controllers/WorkerController.php
@@ -3,15 +3,50 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Worker;
 
 class WorkerController extends Controller
 {
     public function index()
     {
-        return 'this is worker index';
+        $workers = Worker::all();
+        foreach ($workers as $worker) {
+            dump($worker->name);
+        };
     }
 
     public function show(){
         return 'this is worker show';
     }
+    public function create()
+    {
+        $worker = [
+            'name' => 'Mark',
+            'surname' => 'Markov',
+            'email' => 'Markov@mail.ru',
+            'age' => 20,
+            'description' => 'Im Mark',
+            'is_married' => false,
+        ];
+
+        Worker::create($worker);
+
+        return 'Ivan was created';
+    }
+    public function update()
+    {
+        $worker = Worker::find(2);
+        $worker->update([
+            'name' => 'Karl',
+            'surname' => 'Petrov',
+        ]);
+        return 'Was updated';
+    }
+    public function delete()
+    {
+        $worker = Worker::find(2);
+        $worker->delete();
+        return 'Was deleted';
+    }
 }
+
