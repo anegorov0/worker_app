d3a177e84ed694d447529ad384481b3818a9bab3
anegorov0 <signyts@gmail.com>
2025-07-15 18:29:01 +0300

    Добавил фильтры

--- resources/views/workers/index.blade.php
+++ resources/views/workers/index.blade.php
@@ -13,6 +13,23 @@ Index page
         <a href="{{route('workers.create')}}">Добавить</a>
     </div>
     <hr>
+    <div>
+        <form action="{{route('workers.index')}}">
+            <input type="text" name="name" placeholder="name" value="{{request()->get('name')}}">
+            <input type="text" name="surname" placeholder="surname" value="{{request()->get('surname')}}">
+            <input type="text" name="email" placeholder="email" value="{{request()->get('email')}}">
+            <input type="number" name="from" placeholder="from" value="{{request()->get('from')}}">
+            <input type="number" name="to" placeholder="to" value="{{request()->get('to')}}">
+            <input type="text" name="description" placeholder="description" value="{{request()->get('description')}}">
+            <input id="isMarried" type="checkbox" name="is_married" value="{{request()->get('is_married')}}">
+            <label for="isMarried"
+                   {{request()->get('is_married' == 'on' ? 'checked' : '')}}
+            >is married</label>
+            <input type="submit">
+            <a href="{{route('workers.index')}}">Сбросить</a>
+        </form>
+    </div>
+    <hr>
     @foreach($workers as $worker)
         <div>
             <div>Name: {{$worker->name}}</div>
@@ -37,7 +54,7 @@ Index page
         <hr>
     @endforeach
     <div class="my-nav">
-        {{$workers -> links()}}
+        {{$workers ->withQueryString()-> links()}}
     </div>
 </div>
 <style>
